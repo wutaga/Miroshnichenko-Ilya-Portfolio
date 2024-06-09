c36bef41b7e7246fa19087869a0965fe7509bf5d
wutaga <wutaga@gmail.com>
2024-06-09 14:04:56 +0300

    modified 7.1s5

--- python_exercises/Поколение Python курс для начинающих/7  Циклы for и while/7.1step5.py
+++ python_exercises/Поколение Python курс для начинающих/7  Циклы for и while/7.1step5.py
@@ -1,3 +1,6 @@
-n = int(input())
-for i in range(n):
-    print("*"*19)
+a, b = int(input()), int(input())
+total = 0
+for i in range(a, b):
+    if i ** 3 % 4 == 0 or i ** 3 % 9 == 0:
+        total += 1
+print(total)
